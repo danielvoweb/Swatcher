1582515f943b62d6e8cc285e138d2b93da25d7de
danielvoweb <dan@daniel-vo.com>
2015-09-26 22:38:20 -0500

    DRYing up tests by creating shared behaviors

--- Swatcher/tests/integration/specs/swatcherSelectAdvancedColorsTests.js
+++ Swatcher/tests/integration/specs/swatcherSelectAdvancedColorsTests.js
@@ -25,40 +25,7 @@ describe('Example: Colors from a select list using advanced colors', function ()
 
     });
 
-    it('Should hide selected select element', function () {
-
-        $target.swatcher();
-
-        expect($target.is(':visible')).toBeFalsy();
-
-    });
-
-    it('Should create a unordered swatcher list', function () {
-
-        $target.swatcher();
-
-        expect($target.next().prop('tagName')).toBe('UL');
-
-    });
-
-    it('Should add the ".swatcher" class to the unordered list', function () {
-
-        $target.swatcher();
-
-        expect($target.next().hasClass('swatcher')).toBeTruthy();
-
-    });
-
-    it('Should add an item for every option to the swatcher list', function () {
-
-        var expected = $target.find('option').length;
-
-        $target.swatcher();
-
-        expect($target.next().find('li').length).toBe(expected);
-
-    });
-
+    
     it('Should set the background color of each swatch from the "swatcher-color" data attribute', function () {
 
         $target.swatcher();
@@ -67,46 +34,6 @@ describe('Example: Colors from a select list using advanced colors', function ()
 
     });
 
-    it('Should add an ".is-selected" class when a swatch is clicked', function () {
-
-        $target.swatcher();
-
-        $target.next().find('li:first').click();
-
-        expect($target.next().find('li:first').hasClass('is-selected')).toBeTruthy();
-
-    });
-
-    it('Should remove the ".is-selected" class from the current target once another swatch is clicked', function () {
-
-        $target.swatcher();
-
-        $target.next().find('li:first').click();
-
-        $target.next().find('li:nth-child(2)').click();
-
-        expect($target.next().find('li:first').hasClass('is-selected')).toBeFalsy();
-
-    });
-
-    it('Should add a title for each swatch from the option value', function () {
-
-        var expected = $target.find('option:first').val();
-
-        $target.swatcher();
-
-        expect($target.next().find('li:first').prop('title')).toBe(expected);
-
-    });
-
-    it('Should select the corresponding option when a swatch is selected', function () {
-
-        $target.swatcher();
-
-        $target.next().find('li:first').click();
-
-        expect($target.val()).toBe($target.next().find('li:first').prop('title'));
-
-    });
+    sharedSwatcherTests();
 
 });
---
+++ Swatcher/tests/integration/specs/swatcherTests.js
@@ -0,0 +1,93 @@
+﻿'use strict';
+
+function sharedSwatcherTests() {
+
+    var $target;
+
+    beforeEach(function () {
+
+        $target = $('#test-workspace').find('select');
+
+    });
+
+    describe('(shared)', function () {
+
+        it('Should hide selected select element', function () {
+
+            $target.swatcher();
+
+            expect($target.is(':visible')).toBeFalsy();
+
+        });
+
+        it('Should create a unordered swatcher list', function () {
+
+            $target.swatcher();
+
+            expect($target.next().prop('tagName')).toBe('UL');
+
+        });
+
+        it('Should add the ".swatcher" class to the unordered list', function () {
+
+            $target.swatcher();
+
+            expect($target.next().hasClass('swatcher')).toBeTruthy();
+
+        });
+
+        it('Should add an item for every option to the swatcher list', function () {
+
+            var expected = $target.find('option').length;
+
+            $target.swatcher();
+
+            expect($target.next().find('li').length).toBe(expected);
+
+        });
+
+        it('Should add an ".is-selected" class when a swatch is clicked', function () {
+
+            $target.swatcher();
+
+            $target.next().find('li:first').click();
+
+            expect($target.next().find('li:first').hasClass('is-selected')).toBeTruthy();
+
+        });
+
+        it('Should remove the ".is-selected" class from the current target once another swatch is clicked', function () {
+
+            $target.swatcher();
+
+            $target.next().find('li:first').click();
+
+            $target.next().find('li:nth-child(2)').click();
+
+            expect($target.next().find('li:first').hasClass('is-selected')).toBeFalsy();
+
+        });
+
+        it('Should add a title for each swatch from the option value', function () {
+
+            var expected = $target.find('option:first').val();
+
+            $target.swatcher();
+
+            expect($target.next().find('li:first').prop('title')).toBe(expected);
+
+        });
+
+        it('Should select the corresponding option when a swatch is selected', function () {
+
+            $target.swatcher();
+
+            $target.next().find('li:first').click();
+
+            expect($target.val()).toBe($target.next().find('li:first').prop('title'));
+
+        });
+
+    });
+
+}
