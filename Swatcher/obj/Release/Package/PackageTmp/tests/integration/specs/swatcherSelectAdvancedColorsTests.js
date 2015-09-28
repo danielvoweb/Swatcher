1c2761808380517968ac2f9065597cd93927ce43
danielvoweb <dan@daniel-vo.com>
2015-09-28 11:49:21 -0500

    Added solution file and updated Github link

---
+++ Swatcher/obj/Release/Package/PackageTmp/tests/integration/specs/swatcherSelectAdvancedColorsTests.js
@@ -0,0 +1,36 @@
+﻿'use strict';
+
+describe('Example: Colors from a select list using advanced colors', function () {
+
+    var $target;
+
+    beforeEach(function (done) {
+
+        loadWorkspace(
+            '#example-from-select-advanced-colors [data-code-fragment]',
+            '/index.html',
+            '/content/js/jquery.swatcher.js');
+
+        $target = $('#test-workspace').find('select').swatcher();
+
+        done();
+
+    });
+
+    afterEach(function (done) {
+
+        done();
+
+        clearWorkspace();
+
+    });
+
+    it('Should set the background color of each swatch from the "swatcher-color" data attribute', function () {
+
+        expect($target.next().find('li:first').css('background-color')).toBe('rgb(207, 242, 126)');
+
+    });
+
+    sharedSwatcherTests();
+
+});
---
+++ Swatcher/obj/Release/Package/PackageTmp/tests/integration/specs/swatcherSelectBasicColorsTests.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+
+describe('Example: Colors from a select list using basic web colors', function () {
+
+    var $target;
+
+    beforeEach(function (done) {
+
+        loadWorkspace(
+            '#example-from-select-basic-colors [data-code-fragment]',
+            '/index.html',
+            '/content/js/jquery.swatcher.js');
+
+        $target = $('#test-workspace').find('select')
+            .swatcher();
+
+        done();
+
+    });
+
+    afterEach(function (done) {
+
+        done();
+
+        clearWorkspace();
+
+    });
+
+    it('Should set the background color of each swatch from the option value', function () {
+
+        expect($target.next().find('li:first').css('background-color')).toBe('rgb(255, 0, 0)');
+
+    });
+
+    sharedSwatcherTests();
+
+});
