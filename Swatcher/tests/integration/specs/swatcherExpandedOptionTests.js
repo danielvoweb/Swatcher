fa7e474d06858870802f54af9ffd34468612a14d
danielvoweb <dan@daniel-vo.com>
2015-09-28 13:13:07 -0500

    Added failing test for expand selected option

--- Swatcher/tests/integration/specs/swatcherExpandedOptionTests.js
+++ Swatcher/tests/integration/specs/swatcherExpandedOptionTests.js
@@ -1,1 +1,42 @@
 ﻿'use strict';
+
+describe('Example: Using the expand selected Swatcher option', function() {
+
+    var $target, $observed;
+
+    beforeEach(function(done) {
+
+        loadWorkspace(
+            '#example-using-expand-selected-option [data-code-fragment]',
+            '/index.html',
+            '/content/js/jquery.swatcher.js');
+
+        $target = $('#test-workspace').find('select').swatcher();
+
+        $observed = $('#test-workspace').find('ul.swatcher');
+
+        done();
+
+    });
+
+    afterEach(function(done) {
+
+        done();
+
+        clearWorkspace();
+
+    });
+
+    it('Should add an ".is-expanded" class when a swatch is clicked', function() {
+
+        $observed.find('li:first').click();
+
+        $observed.find('li:nth-child(2)').click();
+
+        expect($observed.find('li:first').prop('class')).toContain('is-expanded');
+
+    });
+
+    sharedSwatcherTests();
+
+});
