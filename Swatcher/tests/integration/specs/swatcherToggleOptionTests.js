d02e2326243193722012f0ee4798b314fd170b3b
danielvoweb <dan@daniel-vo.com>
2015-09-28 23:45:58 -0500

    Checking in to save and sync.

--- Swatcher/tests/integration/specs/swatcherToggleOptionTests.js
+++ Swatcher/tests/integration/specs/swatcherToggleOptionTests.js
@@ -1,1 +1,23 @@
-﻿
+﻿'use strict';
+
+describe('Example: Using the toggle selected Swatcher option', function () {
+
+    var $target, $observed;
+
+    beforeEach(function (done) {
+
+        loadWorkspace(
+            '#example-using-expand-selected-option [data-code-fragment]',
+            '/index.html',
+            '/content/js/jquery.swatcher.js');
+
+        $target = $('#test-workspace').find('select').swatcher({ expandable: true });
+
+        $observed = $('#test-workspace').find('ul.swatcher');
+
+        done();
+
+    });
+
+
+});
